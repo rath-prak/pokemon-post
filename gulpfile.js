6f0d154e0e40906ae1a09c4bb2b0859d0f2a8bb5
rath-prak <rathanap@gmail.com>
2016-10-03 20:47:24 +1300

    inial setup

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});	
+});
+
+gulp.task('watch', ['browserSync'], function(){
+	gulp.watch('./*.css', browserSync.reload);
+	gulp.watch('./*.html', browserSync.reload);
+});
